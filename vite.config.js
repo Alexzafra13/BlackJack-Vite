a4e6f948f1e03480c580bb46c1150e75ce9f98b3
Alexzafra13 <ale_zafra_13@hotmail.com>
2025-06-30 23:47:24 +0200

    arreglando ruta imagen cartas

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/BlackJack-Vite/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
+  publicDir: 'public'
 })
